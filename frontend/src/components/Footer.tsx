64d3e60ec5ea7219049a3172ab967b394e76bf7f
Roman <info@roman-werner.de>
2025-03-16 19:17:41 +0100

    basic setup

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="fixed bottom-0 w-full bg-transparent text-gray-400 p-3 text-center">
+      <p>
+        <a href="https://github.com/werner-roman/bytetools" target="_blank">
+          GitHub
+        </a>
+      </p>
+    </footer>
+  );
+}
