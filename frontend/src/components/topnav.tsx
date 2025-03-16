a663c18e8ba7399c6a8b6def2ea53d131742f37f
Roman <info@roman-werner.de>
2025-03-16 21:47:31 +0100

    top nav init

--- frontend/src/components/topnav.tsx
+++ frontend/src/components/topnav.tsx
@@ -1,7 +1,12 @@
 export default function TopNav() {
     return (
-      <nav>
+      <nav className="flex w-full items-center justify-between p-5 text-2xl font-bold bg-asphalt-950">
          <div>Bytetools</div>
+         <div className="flex flex-row items-center gap-4">
+        <>
+        
+        </>
+      </div>
       </nav>
     );
   }
