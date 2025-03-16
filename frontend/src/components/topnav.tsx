a92cea5749056f6c0b53c6af07d80e8aeb7443bc
Roman <info@roman-werner.de>
2025-03-16 22:03:20 +0100

    add logo

--- frontend/src/components/topnav.tsx
+++ frontend/src/components/topnav.tsx
@@ -1,12 +1,15 @@
+import logo from "../assets/logo.png";
+
 export default function TopNav() {
-    return (
-      <nav className="flex w-full items-center justify-between p-5 text-2xl font-bold bg-asphalt-950">
-         <div>Bytetools</div>
-         <div className="flex flex-row items-center gap-4">
-        <>
-        
-        </>
+  return (
+    <nav className="flex w-full items-center justify-between p-1 text-2xl font-bold bg-asphalt-950">
+      <div>
+        <img src={logo} className="w-25 h-auto" alt="Vite logo" />
+        <div>Bytetools</div>
       </div>
-      </nav>
-    );
-  }
+      <div className="flex flex-row items-center gap-4">
+        {/* potential signin/signup buttons */}
+      </div>
+    </nav>
+  );
+}
