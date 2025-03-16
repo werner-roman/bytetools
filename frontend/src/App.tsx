d8943b337f300905a5dc0a91c21011f6ffc0b4f6
Roman <info@roman-werner.de>
2025-03-16 20:03:42 +0100

    cleanup and custom colors and footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,11 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen bg-background text-foreground">
-      <header className="p-4">
+    <div>
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
         <p className="text-gray-400">
           Click on the Vite and React logos to learn more
         </p>
-      </header>
       <main className="flex-grow p-4">
         {/* Your main content goes here */}
       </main>
