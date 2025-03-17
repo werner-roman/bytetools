a0ec266e3248ae7b5d90c59ee9b31635c7d7ff72
Roman <info@roman-werner.de>
2025-03-17 13:55:07 +0100

    basic layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,18 @@
-import Footer from "./components/Footer";
-import TopNav from "./components/TopNav";
-import ToolCard from "./components/ToolCard";
+import Footer from './components/Footer';
+import TopNav from './components/TopNav';
+import ToolCard from './components/ToolCard';
 
 function App() {
   return (
-    <main>
+    <main className="min-h-screen flex flex-col items-center">
       <TopNav />
       <h1 className="text-2xl font-bold text-center mt-4 text-gray-400">
         Simple & Open-Source
       </h1>
-      <div>
-        <ToolCard />
+      <div className="w-full max-w-md mt-8">
+        <ToolCard title="KMZ-Reverse" description="Reverse the direction of a KMZ file" />
+        <ToolCard title="KMZ-Merger" description="Mege multiple KMZ files into one" />
       </div>
-      <main className="flex-grow p-4">{/* Your main content goes here */}</main>
       <Footer />
     </main>
   );
--- frontend/src/components/ToolCard.tsx
+++ frontend/src/components/ToolCard.tsx
@@ -1,3 +1,17 @@
-export default function ToolCard() {
-  return <>Hi</>;
+import React from 'react';
+
+interface ToolCardProps {
+  title: string;
+  description: string;
 }
+
+const ToolCard: React.FC<ToolCardProps> = ({ title, description }) => {
+  return (
+    <div className={`border-2 border-gray-400 rounded-lg p-4 mb-8`}>
+      <h2 className="text-xl font-bold mb-2">{title}</h2>
+      <p className="text-sm text-gray-400">{description}</p>
+    </div>
+  );
+};
+
+export default ToolCard;
